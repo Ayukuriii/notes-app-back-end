569db1358e85a09cb7b807248697bf3d659c0ca7
Ekadian Haris <ekadianharis@gmail.com>
2024-01-13 10:45:43 +0700

    fix bugs on uploads

--- src/api/uploads/handler.js
+++ src/api/uploads/handler.js
@@ -1,7 +1,11 @@
+const autoBind = require('auto-bind')
+
 class UploadsHandler {
   constructor(service, validator) {
     this._service = service
     this._validator = validator
+
+    autoBind(this)
   }
 
   async postUploadImageHandler(request, h) {
